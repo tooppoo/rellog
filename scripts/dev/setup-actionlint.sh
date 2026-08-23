d097ac1f4c54d3151eb0e9dd19dab6e54223ba14
philomagi <tooppoo56@gmail.com>
2026-08-23 17:57:34 +0000

    setup enozunu

--- scripts/dev/setup-actionlint.sh
+++ scripts/dev/setup-actionlint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-sudo bash <(curl -fsSL https://raw.githubusercontent.com/rhysd/actionlint/main/scripts/download-actionlint.bash) \
+curl -fsSL https://raw.githubusercontent.com/rhysd/actionlint/main/scripts/download-actionlint.bash > tmp/install-actionlint.bash
+
+sudo bash tmp/install-actionlint.bash \
   latest \
   "/usr/local/bin"
 
